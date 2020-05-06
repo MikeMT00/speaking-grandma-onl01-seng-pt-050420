534cd077d91d32058c35ba225bbc2e2499a8e634
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-06 15:17:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   if phrase == "Hi Nana, how are you?"
-     return "HUH!? SPEAK UP SONNY!"
+    return "HUH!? SPEAK UP SONNY!"
   end
 end
 

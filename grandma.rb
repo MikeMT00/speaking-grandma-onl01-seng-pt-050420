e012cbe0361c06501a1b8eaefbeeef7d4337e693
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-06 15:11:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 def speak_to_grandma(phrase)
-  if phrase == "Hi Nana, how are you?"
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
+  elsif phrase.downcase
+     phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else
     return "HUH!? SPEAK UP SONNY!"
